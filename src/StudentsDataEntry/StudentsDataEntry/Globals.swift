aaea86fc51b811a2e1f244ed90b0a62f4c947016
Christopher Lord <chris.lord@conkergroup.com>
2016-09-18 13:09:41 +0100

    Creating project-wide static properties to isolate magic values

---
+++ src/StudentsDataEntry/StudentsDataEntry/Globals.swift
@@ -0,0 +1,15 @@
+//
+//  Globals.swift
+//  StudentsDataEntry
+//
+//  Created by Conker Group on 18/09/2016.
+//  Copyright © 2016 Christopher Lord. All rights reserved.
+//
+
+import Foundation
+
+
+class Globals {
+	public static let JSONContentType = "application/json"
+	public static let StudentsUploadEndpoint = "https://posttestserver.com/post.php"
+}
